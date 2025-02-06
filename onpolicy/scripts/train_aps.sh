9c5c8debe414cae255d1d798970296565ee7f7b8
mhmd97z <mmd1997z@gmail.com>
2025-02-06 13:33:12 +0000

    fix: mobility

--- onpolicy/scripts/train_aps.sh
+++ onpolicy/scripts/train_aps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 env="aps"
 algo="mappo"
-exp="comp/scen1_gnnmappo"
+exp="comp_mob_ped/gnnmappo"
 seed_max=1
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
@@ -11,8 +11,8 @@ do
     python train_aps.py --use_valuenorm \
     --env_name ${env} --algorithm_name ${algo} \
     --experiment_name ${exp} --seed ${seed} --n_training_threads 16 --n_rollout_threads 16 \
-    --num_mini_batch 1 --episode_length 10 --num_env_steps 300000 \
+    --num_mini_batch 1 --episode_length 200 --num_env_steps 300000 \
     --ppo_epoch 5 --use_ReLU --lr 7e-4 --critic_lr 7e-4 \
     --user_name "marl" --use_recurrent_policy False --max_grad_norm 1 \
-    --gamma 0.01 --use_linear_lr_decay --max_grad_norm 1
+    --gamma 0.01 --use_linear_lr_decay
 done
