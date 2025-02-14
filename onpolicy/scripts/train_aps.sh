9f3a230b9ec91210fbf6780380394004f36885b0
mhmd97z <mmd1997z@gmail.com>
2025-02-14 14:56:14 +0000

    feat: new power model

--- onpolicy/scripts/train_aps.sh
+++ onpolicy/scripts/train_aps.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 env="aps"
 algo="mappo"
-exp="comp_mob_ped/gnnmappo"
+exp="updated_power_apconst05/ped/gnnmappo/pcoef1_localpsum1_conncost0"
 seed_max=1
 
-echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
     python train_aps.py --use_valuenorm \
     --env_name ${env} --algorithm_name ${algo} \
     --experiment_name ${exp} --seed ${seed} --n_training_threads 16 --n_rollout_threads 16 \
-    --num_mini_batch 1 --episode_length 200 --num_env_steps 300000 \
+    --num_mini_batch 1 --episode_length 100 --num_env_steps 160000 \
     --ppo_epoch 5 --use_ReLU --lr 7e-4 --critic_lr 7e-4 \
     --user_name "marl" --use_recurrent_policy False --max_grad_norm 1 \
-    --gamma 0.01 --use_linear_lr_decay
+    --gamma 0.01 --use_linear_lr_decay --log_interval 1
 done
